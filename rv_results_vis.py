06c6e34a88c8fc1734215e8bd6b60ca36971d9e0
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-03 13:32:22 -0400

    Plots new version of visualization, name and spec type printed, without top/right axes

--- rv_results_vis.py
+++ rv_results_vis.py
@@ -1,14 +1,14 @@
+import avg_rv
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import NIRSPEC
-import avg_rv
+from matplotlib.ticker import FormatStrFormatter
 
-
-def rv_vis(path_to_rv_results_minus_df, obj_spec_type=30):
+def rv_vis(path_to_rv_results_minus_df,tar_spec_type):
     df = pd.read_csv(path_to_rv_results_minus_df, sep='\t')
 
     plt.clf()
+
+    ax = plt.subplot(111)
 
     for i, row in df.iterrows():
         spec_type = row['std_opt_spec_type']
@@ -18,37 +18,45 @@ def rv_vis(path_to_rv_results_minus_df, obj_spec_type=30):
 
         spec_type = float(spec_type)
 
-        plt.scatter(spec_type, row['obj_rv'], color='black')
-
-        plt.errorbar(spec_type, row['obj_rv'], yerr=row['obj_unc'], color='black', alpha=0.7)
+        ax.scatter(spec_type,row['obj_rv'],color='black')
+        ax.errorbar(spec_type, row['obj_rv'], yerr=row['obj_unc'],color='black',alpha=0.7)
 
     beginning_of_path = path_to_rv_results_minus_df.split('/')[6]
     obj_name = beginning_of_path.split('_')[0]
     # 	print obj_name
 
-    rv, unc = avg_rv.avg_rv(path_to_rv_results_minus_df)
+    calc_rv_unc = avg_rv.avg_rv(path_to_rv_results_minus_df)
+    rv = calc_rv_unc[0]
+    unc = calc_rv_unc[1]
 
-    obj_spec_type = obj_spec_type.replace('M', '0').replace('L', '1').replace('T', '2')
-    obj_spec_type = float(obj_spec_type)
+    plt.annotate(str(obj_name)+' '+str(tar_spec_type), xycoords='axes fraction', xy=(.72, .05), size=15)
+    # eleven = float(13)
+    # obj_spec_type_position = float(obj_spec_type) - 5.0
+    # xmax = obj_spec_type_position / eleven
 
-    plt.scatter(obj_spec_type, rv, marker='*', s=200, color='blue')
-    plt.annotate(str(obj_name), xycoords='axes fraction', xy=(.77, .05), size=15)
-    eleven = float(13)
-    obj_spec_type_position = float(obj_spec_type) - 5.0
-    xmax = obj_spec_type_position / eleven
-
-    plt.xlim([5, 18])
-    plt.ylim([-1.5, 6.5])
+    ax.set_xlim([5, 18])
+    ax.set_ylim([-1.5, 6.5])
     # 	plt.axhline(y=rv, xmax=xmax, color='blue', ls='--',alpha=0.5)
-    plt.fill_between(range(5, 19), rv + unc, rv - unc, interpolate=True, alpha=0.3)
+    ax.fill_between(range(5, 19), rv + unc, rv - unc, interpolate=True, alpha=0.3, color='grey')
 
     spec_type_range = [6, 8, 10, 12, 14, 16, 18]
     labels = ['M6', 'M8', 'L0', 'L2', 'L4', 'L6', 'L8']
-    plt.xticks(spec_type_range, labels)
+    ax.set_xticks(spec_type_range)
+    ax.set_xticklabels(labels)
 
-    plt.xlabel('Spectral Type')
-    plt.ylabel('Calculated RV')
+    y_axis_min = ax.dataLim.bounds[1]
+    y_axis_max = ax.dataLim.bounds[3]
 
-    plt.savefig(str(obj_name) + '_calculated_rvs.png')
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+    y_axis_ticks=[y_axis_min, rv-unc, rv, rv+unc, y_axis_max]
+    ax.set_yticks(y_axis_ticks)
+
+    ax.set_xlabel("Spectral Type of RV Standard")
+    ax.set_ylabel('Calculated RV (km/s)')
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    # plt.savefig(str(obj_name) + '_rv_results.png')
 
     plt.show()
