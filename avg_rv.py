d03989d1e97a2ff0372f4ac02320afab8e256f0e
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-03 11:46:51 -0400

    First commit

---
+++ avg_rv.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import weighted_stddev
+
+
+def avg_rv(path_to_df):
+    df = pd.read_csv(path_to_df, sep='\t')
+
+    rvs = []
+    uncs = []
+
+    for i, row in df.iterrows():
+        rvs.append(row['obj_rv'])
+        uncs.append(row['obj_unc'])
+
+    rvs = np.asarray(rvs)
+    uncs = np.asarray(uncs)
+
+    rv, unc = weighted_stddev(rvs, uncs)
+
+    print("Avg RV = " + str(rv) + " +/- " + str(unc))
