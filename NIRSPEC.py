54e039d9a1467da32de0d3fba21c194429e0edcc
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-10 11:47:13 -0400

    Added docstrings and comments

--- NIRSPEC.py
+++ NIRSPEC.py
@@ -1,13 +1,11 @@
 import math
-import numpy
-import find_rv
-from astropy.io import ascii
-from astropy import coordinates
-from astropy import time
-from astropy import units
-#from astrolib import baryvel
 import sys
-import matplotlib.pyplot as plt
+
+import numpy
+from astropy.io import ascii
+
+import find_rv
+
 
 def deg2HMS(input):
     deg = int(input)
@@ -58,6 +56,12 @@ def clip(a,start,end):
     return numpy.asarray(out)
 
 def main(argv=None):
+    """Uses find_rv to calculated a RV using NIRSPEC data.
+
+    Input: a list, ex: ['','std_path',rv_std,rv_std_err,'obj_path',crop_usually_1,xcorr_width_optional,cutstart_optional,cutend_optional]
+
+    Output: Figure of find_rv iterations, textfile of code results, prints calculated RV and uncertainty"""
+
     if argv is None:
         argv = sys.argv
         
