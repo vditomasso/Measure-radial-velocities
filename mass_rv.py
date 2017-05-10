9095c49abe7b3972f5a7d50395a3f291f02ac58a
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-10 10:51:59 -0400

    Made mass_rv_outliers a function in mass_rv

--- mass_rv.py
+++ mass_rv.py
@@ -18,3 +18,21 @@ def mass_rv(obj_filename, obj_name, path_to_df):
     df['obj_unc'] = errors
 
     df.to_csv(str(obj_name) + '_against_all_comps.txt', sep='\t')
+
+def mass_rv_outliers(obj_filename, obj_name, path_to_df):
+    df = pd.read_csv(path_to_df, sep='\t')
+
+    rvs = []
+    errors = []
+
+    for i, row in df.iterrows():
+        rv, error = NIRSPEC.main(
+            ['', row['filename'], row['std_rv'], row['std_unc'], obj_filename, 0, 200, row['lower_pixel_shift'],
+             row['upper_pixel_shift']])
+        rvs.append(rv)
+        errors.append(error)
+
+    df['obj_rv'] = rvs
+    df['obj_unc'] = errors
+
+    df.to_csv(str(obj_name) + '_against_all_outliers.txt', sep='\t')
