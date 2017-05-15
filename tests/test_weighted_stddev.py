5809d6a54997f737b8696ae00ff34f3fbdd6995e
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-15 18:49:09 -0400

    Added assert 1 == 1 test

--- tests/test_weighted_stddev.py
+++ tests/test_weighted_stddev.py
@@ -2,3 +2,6 @@ from Measure-radial-velocities.weighted_stddev import wstddev
 
 def test_weighted_stddev_1():
     assert wstddev([5,6,7],[1,2,1]) == (6.0, 1.0)
+
+def test_weighted_stddev_2():
+    assert 1 == 1
