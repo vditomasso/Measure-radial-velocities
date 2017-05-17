bd8bc0d8d41bb62f0bd6d7d1fa8ff31ebabd30cb
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-17 10:53:45 -0400

    Moved setup.py up a level

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+setup(
+    name="Measure-radial-velocities",
+    version="0.1",
+    py_modules=['avg_rv','find_rv','FIRE','mass_rv','NIRSPEC','rv_results_vis','weighted_stddev']
+)
