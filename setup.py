ea6730ccd4ecd66e144f7e0dbfff66174f976f3e
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-17 10:56:43 -0400

    Put packages=['Measure-radial-velocities'] in setup.py file

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name="Measure-radial-velocities",
     version="0.1",
-    py_modules=['avg_rv','find_rv','FIRE','mass_rv','NIRSPEC','rv_results_vis','weighted_stddev']
+    packages=['Measure-radial-velocities']
 )
