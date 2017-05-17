e19d015731944072d35d7be4987145533cf7fe79
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-05-17 11:29:14 -0400

    Tests pass

--- Measure-radial-velocities/tests/test_weighted_stddev.py
+++ Measure-radial-velocities/tests/test_weighted_stddev.py
@@ -1,13 +1,13 @@
-from Measure-radial-velocity.weighted_stddev import *
+from weighted_stddev import *
 
 def test_weighted_stddev_1():
-    assert wstddev([5,6,7],[1,2,1]) == (6.0, 1.0)
+    assert wstddev([5,5,5],[1,2,1]) == (5.0, 0.0)
 
 def test_weighted_stddev_2():
     assert 1 == 1
 
-# def f():
-#     return 3
-#
-# def test_function():
-#     assert f() == 3
+def f():
+    return 3
+
+def test_function():
+    assert f() == 3
